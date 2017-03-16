4e08c5ff80d7f9521555a0b48a1e9f050773d6a8
Mark Seemann <mark@ploeh.dk>
2017-03-16 15:00:16 +0100

    Added initial version of Furl
    This version is the result of a couple of months' of prototyping and
    tweaking. It supports exactly the feature set I've needed so far, which
    is GET and POST requests, and being able to specify the Content-Type
    header for a request.
    
    Lots of other scenarios (such as PUT and DELETE) requests can be added
    later, but so far, I haven't needed them.

---
+++ Furl.fsx
@@ -0,0 +1,41 @@
+﻿#r "System.Net.Http"
+
+open System
+open System.Net.Http
+
+let private addHeader (headers : Headers.HttpHeaders) (name, value : string) =
+    headers.Add (name, value)
+
+let private addBody (req : HttpRequestMessage) headers body =
+    req.Content <- new StringContent (body)
+    let contentTypeHeader =
+        headers |> List.tryFind (fun (n, _) -> n = "Content-Type")
+    contentTypeHeader
+    |> Option.iter (fun (_, v) -> req.Content.Headers.ContentType.MediaType <- v)
+
+let result (t : System.Threading.Tasks.Task<_>) = t.Result
+
+let composeMessage meth (url : Uri) headers body =
+    let req = new HttpRequestMessage (meth, url)
+    Option.iter (addBody req headers) body
+
+    headers
+    |> List.partition (fun (n, _) -> n = "Content-Type")
+    |> snd
+    |> List.iter (addHeader req.Headers)
+    req
+
+let get url headers =
+    use client = new HttpClient ()
+    composeMessage Net.Http.HttpMethod.Get (Uri url) headers None
+    |> client.SendAsync
+    |> result
+
+let post url headers body =
+    use client = new HttpClient ()
+    composeMessage Net.Http.HttpMethod.Post (Uri url) headers (Some body)
+    |> client.SendAsync
+    |> result
+
+let bodyText (resp : HttpResponseMessage) =
+    resp.Content.ReadAsStringAsync().Result
